7cc0082d385842c1fb1f25bc4ac37199f7348968
TNTaabb234 <ku.yeh1207@gmail.com>
2020-08-28 19:28:41 +0800

    Just added a lttle command

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const dc = require('discord.js')
 const client = new dc.Client()
 const token = require('./token.json')
 const config = require('./config.json')
+const fs = require('fs')
 
 client.on('ready', () => {
     console.log(`已登錄${client.user.tag}`)
     console.log(`Running on Discord.JS ${dc.version}`)
 })
 //command listener
-client.on('message', async => {
+client.on('message', async message => {
     if(message.author.bot) return
-    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const args = message.content.slice(config.prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
+
+    if(command === "test") {
+        message.channel.bulkDelete(1)
+        message.reply("YEEE")
+        console.log(`Runned commind in ${message.guild}`)
+    }
+
+
 })
 
 client.login(token.token)
